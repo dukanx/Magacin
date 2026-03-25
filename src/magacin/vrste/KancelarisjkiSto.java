1b93a6a91aeb2ec5ea890f8512f39b44a9d3900d
dukanx <nikoladukic04@gmail.com>
2026-03-25 01:07:45 +0100

    dodati equals, hashCode i toString

--- src/magacin/vrste/KancelarisjkiSto.java
+++ src/magacin/vrste/KancelarisjkiSto.java
@@ -1,5 +1,33 @@
 package magacin.vrste;
 
-public class KancelarisjkiSto {
+import magacin.Artikal;
 
+public class KancelarisjkiSto extends Artikal{
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "KancelarisjkiSto [hashCode()=" + hashCode() + ", getNaziv()=" + getNaziv() + ", getSifra()="
+				+ getSifra() + ", getOpis()=" + getOpis() + ", getKolicina()=" + getKolicina() + ", toString()="
+				+ super.toString() + ", getClass()=" + getClass() + "]";
+	}
+
+	
 }
--- src/magacin/vrste/KucnaHemija.java
+++ src/magacin/vrste/KucnaHemija.java
@@ -1,8 +1,11 @@
 package magacin.vrste;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-public class KucnaHemija {
+import magacin.Artikal;
+
+public class KucnaHemija  extends Artikal{
 
 	private LocalDateTime rokTranja;
 
@@ -12,6 +15,36 @@ public class KucnaHemija {
 
 	public void setRokTranja(LocalDateTime rokTranja) {
 		this.rokTranja = rokTranja;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(rokTranja);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		KucnaHemija other = (KucnaHemija) obj;
+		return Objects.equals(rokTranja, other.rokTranja);
+	}
+
+	@Override
+	public String toString() {
+		return "KucnaHemija [rokTranja=" + rokTranja + ", getRokTranja()=" + getRokTranja() + ", hashCode()="
+				+ hashCode() + ", getNaziv()=" + getNaziv() + ", getSifra()=" + getSifra() + ", getOpis()=" + getOpis()
+				+ ", getKolicina()=" + getKolicina() + ", toString()=" + super.toString() + ", getClass()=" + getClass()
+				+ "]";
 	}
 	
 	
